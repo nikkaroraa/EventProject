7ab8b0fd9cc631c42894805662124b58a479ccb7
Nikhil Arora <narora200@gmail.com>
2017-06-18 01:22:23 +0530

    Event can be created now

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,10 +1,36 @@
 const router = require('express').Router();
-const User = require('../../db/models').models.event;
+const Event = require('../../db/models').models.Event;
 
 router.get('/', (req, res)=>{
 	res.send('On /api/event route');
 })
 
 
+router.post('/new', (req, res) => {
+    //Add server-side validations if required here
+    if (!req.body.name) {
+        return res.status(403).send('Event cannot created without name')
+    }
 
+    // YYYY-MM-DD'T'HH:MM
+    Event.create({
+        name: req.body.title,
+        venue: req.body.venue,
+        imgUrl: req.body.imgUrl,
+        startTime: new Date(req.body.startTime),
+        endTime: new Date(req.body.endTime),
+        message: req.body.message,
+		userId: 1 /*req.user.id*/
+
+	})
+	.then((event) => 
+	{
+		res.status(200).send(event);
+	})
+	.catch((err) => 
+	{
+        res.status(500).send("There was an error creating event")
+
+	})
+})
 module.exports = router;
